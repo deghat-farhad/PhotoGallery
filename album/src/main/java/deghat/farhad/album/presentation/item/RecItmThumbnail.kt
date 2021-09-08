f620d1520bb95ff3b368c501af728859deb46d7c
Farhad.Deghat <deghat.farhad@gmail.com>
2021-09-08 15:59:24 +0430

    thumbnile onClick

--- album/src/main/java/deghat/farhad/album/presentation/item/RecItmThumbnail.kt
+++ album/src/main/java/deghat/farhad/album/presentation/item/RecItmThumbnail.kt
@@ -6,8 +6,13 @@ import deghat.farhad.common.presentation.util.recycler_view.Visitable
 sealed class RecItmThumbnail : Visitable {
     data class Thumbnail(
         val url: String,
-        val size: String
+        val size: String,
+        val photoItem: PhotoItem,
+        val onClickAction: ((PhotoItem) -> Unit)
     ) : RecItmThumbnail() {
         override fun type() = R.layout.itm_thumbnail
+        fun onClick() {
+            onClickAction(photoItem)
+        }
     }
 }
--- album/src/main/java/deghat/farhad/album/presentation/mapper/ThumbnailItemMapper.kt
+++ album/src/main/java/deghat/farhad/album/presentation/mapper/ThumbnailItemMapper.kt
@@ -1,18 +1,34 @@
 package deghat.farhad.album.presentation.mapper
 
 import deghat.farhad.album.domain.model.Photo
+import deghat.farhad.album.presentation.item.PhotoItem
 import deghat.farhad.album.presentation.item.RecItmThumbnail
+import java.text.DateFormat
+import java.util.*
 import javax.inject.Inject
 import kotlin.math.pow
 
 class ThumbnailItemMapper @Inject constructor() {
-    fun mapToPresentation(from: Photo) = RecItmThumbnail.Thumbnail(
-        from.thumbnail.url ?: "",
-        if (from.thumbnail.size == null)
-            "---"
-        else
-            "${"%.2f".format(bytesToMegaBytes(from.thumbnail.size))}MB"
-    )
+    companion object {
+        const val DATE_FORMAT = "MMMM yyyy"
+    }
+
+    fun mapToPresentation(from: Photo): RecItmThumbnail.Thumbnail {
+        val format = DateFormat.getDateInstance(DateFormat.LONG, Locale.FRANCE);
+        return RecItmThumbnail.Thumbnail(
+            from.thumbnail.url ?: "",
+            if (from.thumbnail.size == null)
+                "---"
+            else
+                "${"%.2f".format(bytesToMegaBytes(from.thumbnail.size))}MB",
+            PhotoItem(
+                from.fullScreen.url ?: "",
+                from.fullScreen.creationDate?.let {
+                    format.format(it)
+                } ?: "---"
+            )
+        ) {}
+    }
 
     private fun bytesToMegaBytes(bytes: Long) = bytes / 1024f.pow(2)
 }
